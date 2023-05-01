4d36a56946957b9bd6eee6479198b3855f4ee9c3
Ravneet <rsingh@daytranslations.com>
2023-05-01 17:34:47 +0530

    debuging

--- src/App.js
+++ src/App.js
@@ -1,36 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
-import ThirdComoponent,{SecondComoponent,FirstComoponent} from './components';
-import Login from './components/classComponent';
-import Header from './components/header';
-import Child1 from './components/child1';
-import { createContext, useEffect, useState } from 'react';
-import LearnUseeffect from './components/useEffect';
-import Child2 from './components/child2';
-import LoginForm from './components/loginForm';
-import BasicForm from './components/loginForm/formik';
+import logo from "./logo.svg";
+import "./App.css";
+import ThirdComoponent, {
+  SecondComoponent,
+  FirstComoponent,
+} from "./components";
+import Login from "./components/classComponent";
+import Header from "./components/header";
+import Child1 from "./components/child1";
+import { createContext, useEffect, useState } from "react";
+import LearnUseeffect from "./components/useEffect";
+import Child2 from "./components/child2";
+import LoginForm from "./components/loginForm";
+import BasicForm from "./components/loginForm/formik";
 
-const data1=createContext();
-const data2=createContext();
+const data1 = createContext();
+const data2 = createContext();
 
 function App() {
-  const [obj,setObj]=useState({
-    name:"John doe",
-    age:12
-  })
- 
+  const [obj, setObj] = useState({
+    name: "John doe",
+    age: 12,
+  });
+
   // useEffect(()=>{
-   
+
   //   setObj({name:"Ajay"})
 
   // },[obj])
 
-  
-   setTimeout(()=>{
-    setObj({name:"Ajay"})
-    },5000)
+  setTimeout(() => {
+    setObj({ name: "Ajay" });
+  }, 5000);
 
-  const address="123,park street, California"
+  const address = "123,park street, California";
 
   return (
     <div className="App">
@@ -50,11 +52,11 @@ function App() {
       {/* <LearnUseeffect/> */}
 
       {/* <LoginForm/> */}
-<BasicForm/>      
+      <BasicForm />
     </div>
   );
 }
 
 export default App;
 
-export {data1,data2}
+export { data1, data2 };
